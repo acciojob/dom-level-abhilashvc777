89a22d3dd55fdff01c6e5153f7681f629a558f9c
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-20 14:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const ul = document.querySelector("ul")
+const li = document.querySelectorAll("ul li")
+let n;
+for(let i=0; i<li.length;i++){
+	if(li[i].id==="level"){
+		n=i
+	}
+}
+alert(`The level of the element is: ${n}`)
